7f1821c1357dbe97b424af9b273e6ce49eb84c88
Alfonso Gerez <alfonsoogerez@gmail.com>
2023-01-30 21:48:56 -0300

    primer commit desde terminal

---
+++ fuction.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById('dark').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#2C3E50';
+    document.body.style.color = 'white'
+})
+
+document.getElementById('claro').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'rgba(255, 255, 255, 0.747)';
+    document.body.style.color = 'black'
+})
